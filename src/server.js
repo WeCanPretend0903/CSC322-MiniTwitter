28bc838b14f1dbe43a3f87d820b987b768a7cdf0
ReaganD1 <rdhar99999@gmail.com>
2023-11-29 15:54:57 -0500

    changes

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require('./config/db');
+
+const app = require('express')();
+const port = process.env.PORT || 3000;
+
+const UserRouter = require('./api/user');
+
+const bodyParser = require('express').json;
+app.user(bodyParser());
+app.use('/user', UserRouter)
+
+app.listen(port, () => {
+    console.log(`Server running on Port ${port}`);
+})
